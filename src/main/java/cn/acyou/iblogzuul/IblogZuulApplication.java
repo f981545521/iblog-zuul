a4bfe9de852db78133cfdcfb123f374715ed9c0a
youfang <981545521@qq.com>
2018-06-22 16:21:05 +0800

    init import
    init import

---
+++ src/main/java/cn/acyou/iblogzuul/IblogZuulApplication.java
@@ -0,0 +1,12 @@
+package cn.acyou.iblogzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IblogZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IblogZuulApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/acyou/iblogzuul/IblogZuulApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.acyou.iblogzuul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IblogZuulApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
